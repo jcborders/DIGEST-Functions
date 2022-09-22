aec68d054ac96f60863530f64e3fd2a48cec7f32
jcborders <jamesborders91@gmail.com>
2022-09-22 17:05:58 -0400

    Update function based on JC edits

--- digest_safety_function_v2.R
+++ digest_safety_function_v2.R
@@ -239,12 +239,13 @@ digest_safety <- function(data, # put data frame title here
            pas_7_8_frequency = case_when(pas == 7 | pas == 8 & IDDSI == 0 ~ 1,
                                          pas == 7 | pas == 8 & IDDSI > 0 ~ 1),
            intermittent_IDDSI_0 = IDDSI_0_freq/IDDSI_0_trials,
-           intermittent_IDDSI_greater_0 = (IDDSI_4_freq + IDDSI_7_freq)/IDDSI_4_trials
+           intermittent_IDDSI_4 = IDDSI_4_freq/IDDSI_4_trials,
+           intermittent_IDDSI_7 = IDDSI_7_freq/IDDSI_7_trials
     ) |> 
     top_n(1, pas) |>  # keep rows based on max PAS
     mutate( # calculate max values for VF and subglottis for when > 2 trials with same max PAS score
       vocal_folds_severity_rating_max = case_when(
-        max_pas >= 5 | max_pas <= 6 ~ max(vocal_folds_severity_rating)),
+        max_pas >= 5 | max_pas <= 6 ~ max(vocal_folds_severity_rating)), # is this combo of VF or subglottis or deepest landmark???
       subglottis_severity_rating_max = case_when(max_pas >= 7 | max_pas <= 8 ~ max(subglottis_severity_rating))
     ) |> 
     dplyr::slice(1) |> 
@@ -252,15 +253,15 @@ digest_safety <- function(data, # put data frame title here
       max_pas == 1 | max_pas == 2 ~ 0,
       freq_airway_invasion == 1 & max_pas == 3 | max_pas == 4 ~ 0, # PAS 3-4, single event
       freq_airway_invasion > 1 & max_pas == 3 | max_pas == 4 ~ 1, # PAS 3-4, multiple events
-      freq_airway_invasion == 1 & vocal_folds_severity_rating_max <= 25 & max_pas == 5 | max_pas == 6 ~ 1, # PAS 5-6, single, not gross
-      freq_airway_invasion == 1 & vocal_folds_severity_rating_max > 25 & max_pas == 5 | max_pas == 6 ~ 2, # PAS 5-6, single event, gross
+      freq_airway_invasion == 1 & vocal_folds_severity_rating_max <= .25 & max_pas == 5 | max_pas == 6 ~ 1, # PAS 5-6, single, not gross
+      freq_airway_invasion == 1 & vocal_folds_severity_rating_max > .25 & max_pas == 5 | max_pas == 6 ~ 2, # PAS 5-6, single event, gross - CHECK THIS
       freq_airway_invasion > 1 & max_pas == 5 | max_pas == 6 ~ 2, # PAS 5-6, multiple events
-      freq_airway_invasion == 1 & subglottis_severity_rating_max < 25 & freq_5_6 < 1 & max_pas == 7 | max_pas == 8 ~ 1, # PAS 7-8, single event, not gross, no PAS 5 or 6
-      freq_airway_invasion == 1 & subglottis_severity_rating_max < 25 & freq_5_6 >= 1 & max_pas == 7 | max_pas == 8 ~ 2, # PAS 7-8, single event, not gross, additional PAS 5 or 6's
-      freq_airway_invasion > 1 & intermittent_IDDSI_0 < 0.50 | intermittent_IDDSI_0 < 0.50 & subglottis_severity_rating_max < 25 & max_pas == 7 | max_pas == 8 ~ 2, # PAS 7-8, less than 50%, not gross
-      freq_airway_invasion > 1 & intermittent_IDDSI_0 >= 0.50 | chronic == 1 & subglottis_severity_rating_max < 25 & max_pas == 7 | max_pas == 8 ~ 3, # PAS 7-8, chronic, not gross
-      freq_airway_invasion >= 1 & intermittent_IDDSI_0 < 0.50 | intermittent_IDDSI_0 < 0.50 & subglottis_severity_rating_max >= 25 & max_pas == 7 | max_pas == 8 ~ 3, # PAS 7-8, less than 50%, gross
-      freq_airway_invasion > 1 & intermittent_IDDSI_0 >= 0.50 | chronic == 1 & subglottis_severity_rating_max >= 25 & max_pas == 7 | max_pas == 8 ~ 4 # PAS 7-8, more than 50%, gross
+      freq_airway_invasion == 1 & subglottis_severity_rating_max <= .25 & freq_5_6 < 1 & max_pas == 7 | max_pas == 8 ~ 1, # PAS 7-8, single event, not gross, no PAS 5 or 6
+      freq_airway_invasion == 1 & subglottis_severity_rating_max <= .25 & freq_5_6 >= 1 & max_pas == 7 | max_pas == 8 ~ 2, # PAS 7-8, single event, not gross, additional PAS 5 or 6's
+      freq_airway_invasion > 1 & intermittent_IDDSI_0 < 0.50 | intermittent_IDDSI_4 < 0.50 | intermittent_IDDSI_4 < 0.50 & subglottis_severity_rating_max <= .25 & max_pas == 7 | max_pas == 8 ~ 2, # PAS 7-8, less than 50%, not gross
+      freq_airway_invasion > 1 & intermittent_IDDSI_0 >= 0.50 | chronic == 1 & subglottis_severity_rating_max <= .25 & max_pas == 7 | max_pas == 8 ~ 3, # PAS 7-8, chronic, not gross
+      freq_airway_invasion >= 1 & intermittent_IDDSI_0 < 0.50 | intermittent_IDDSI_0 < 0.50 & subglottis_severity_rating_max > .25 & max_pas == 7 | max_pas == 8 ~ 3, # PAS 7-8, less than 50%, gross
+      freq_airway_invasion > 1 & intermittent_IDDSI_0 >= 0.50 | chronic == 1 & subglottis_severity_rating_max > .25 & max_pas == 7 | max_pas == 8 ~ 4 # PAS 7-8, more than 50%, gross
     )
     ) |> 
     select(id, max_pas, safety_score)
